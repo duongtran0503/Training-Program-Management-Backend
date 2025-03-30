4aa3fce1d324794c8f71f1dc902700f014eb5ff6
xiaohu <duonght2004.itsgu@gmail.com>
2025-03-30 21:11:20 +0700

    init source code

---
+++ src/main/java/com/trainingapi/trainingAPi/TrainingAPiApplication.java
@@ -0,0 +1,13 @@
+package com.trainingapi.trainingAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainingapi/trainingAPi/TrainingAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingapi.trainingAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
